3c45546e2f71236a0e4ebcb38103f0268997fecc
zz1601994913 <67530434+zz1601994913@users.noreply.github.com>
2021-06-05 09:47:26 +0800

    Add files via upload

---
+++ 5-SHORTEST PATHS：LABEL-CORRECTING/lable_correcting_fifo.py
@@ -0,0 +1,29 @@
+M = 99
+A = [
+    [M, 2, 2, M, M],
+    [M, M, M, 4, 3],
+    [M, M, M, M, 1],
+    [M, M, M, M, 3],
+    [M, M, M, M, M]
+]
+s = 1
+
+
+def lable_correcting_fifo(A, s):
+    d = [99] * len(A)
+    pred = [0] * len(A)
+    d[s - 1] = 0
+    LIST = [s]  # list中存标号
+    while LIST:
+        i = LIST.pop(0)
+        for j in range(len(A)):
+            if d[j] > d[i - 1] + A[i - 1][j]:
+                d[j] = d[i - 1] + A[i - 1][j]
+                pred[j] = i
+                LIST.append(j + 1)
+    return d, pred
+
+
+[d, pred] = lable_correcting_fifo(A, s)
+print(d)
+print(pred)
